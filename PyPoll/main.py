fedcef55bb7d227a179ebb05cd31b89fa0cc0ae2
SearsChris <searschris83@gmail.com>
2020-10-12 16:47:05 -0500

    working poll need formatting

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,11 @@
 import os
 import csv
-
+# point to Election data csv
 poll_csv = os.path.join("Resources", "03-python_HW_Instructions_PyPoll_Resources_election_data.csv")
 
 vote_count = 0
 vote = 0
-
+#create null dictionary
 candidlist = {}
 
 with open(poll_csv) as csvfile:
@@ -19,9 +19,16 @@ with open(poll_csv) as csvfile:
         else:
             candidlist[candidate] += 1
     print(candidlist)
+    CandidateName = ""
+    winner = 0
     for candidate in candidlist:
         print(candidate)
         vote_percent = (candidlist[candidate]/vote_count) *100
-
         print(vote_percent)
+        #candidate with most votes wins.
+        if candidlist[candidate] > winner:
+            winner = candidlist[candidate]
+            CandidateName = candidate
+    print(CandidateName + str(winner))
+    
 
