0dba8747ecae4ea4a032f96ea1aeeadf23ebed50
SearsChris <searschris83@gmail.com>
2020-10-12 00:24:51 -0500

    12:24am

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,32 @@
+
 import os
 import csv
+# define variables
 
-bank_csv = os.path.join('Resources', 'Budget_data.csv')
-
-with open(bank_csv) as csvfile:
+bankcsv = 'Resources/Budgetdata.csv'
+month_count = 0
+netprofit = 0
+with open(bankcsv) as csvfile:
     
-    csvreader = csv.reader(bank_csv, delimiter=",")
-    csv_header = next(csvreader)
-    print(csvreader)
+    BankData = csv.reader(csvfile, delimiter=",")
+    csv_header = next(BankData)
+    print(BankData)
 
-    for row in csvreader:
-        print(row[0])
+    for row in BankData:
+        month_count = float(month_count + 1)
+        netprofit = float(row[1])
+        average = netprofit/month_count
+        #netprofit = "${:,.2f}".format(netprofit)
+        #average = "${:,.2f}".format(average)
+    print('Total Months:', month_count)
+    print('Total Net Profit:',netprofit)
+    print('Aberage Change:', '$',average)
+   # print('Total Profits:', netprofit)
 
 
-#total_months =
+
+    #total_months = str(len(BankData))
+    #print(BankData)
 #net_total = 
 #avg_chage = 
 #greatest_increase = 
