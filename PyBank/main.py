ab9a403f93a87de0bf6ab213391ee19437cfca4b
SearsChris <searschris83@gmail.com>
2020-10-12 10:29:04 -0500

    average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,31 +3,32 @@ import os
 import csv
 # define variables
 
-bankcsv = 'Resources/Budgetdata.csv'
+bankcsv = os.path.join('Resources/Budgetdata.csv')
 month_count = 0
+monthlyprofit = 0
 netprofit = 0
+
 with open(bankcsv) as csvfile:
     
     BankData = csv.reader(csvfile, delimiter=",")
     csv_header = next(BankData)
-    print(BankData)
+    line_count = 0
 
     for row in BankData:
-        month_count = float(month_count + 1)
-        netprofit = float(row[1])
-        average = netprofit/month_count
+        #print(row[0], row[1])
+        #line_count += 1
+        month_count = month_count + 1
+        change = row[1]
+        #netprofit = float(row[1])
+        for change in row:
+             int(row[1]) - int(row[1])
         #netprofit = "${:,.2f}".format(netprofit)
         #average = "${:,.2f}".format(average)
+        #print(change)
+        #line_count += 1
+        avgchange = int(change)/int(month_count)
     print('Total Months:', month_count)
-    print('Total Net Profit:',netprofit)
-    print('Aberage Change:', '$',average)
-   # print('Total Profits:', netprofit)
-
-
-
-    #total_months = str(len(BankData))
-    #print(BankData)
-#net_total = 
-#avg_chage = 
-#greatest_increase = 
-#greatest_decrease = 
+    #print('Total Net Profit:',netprofit)
+    #print('Average Change:', '$',average)
+    #print('Total Profits:', netprofit)
+print('Average Change:', avgchange)
