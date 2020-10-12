eefc6c966310d0fe8d293ffb45508422618e5e60
SearsChris <searschris83@gmail.com>
2020-10-11 22:44:35 -0500

    update 1

---
+++ PyBank/main.py
@@ -0,0 +1,20 @@
+import os
+import csv
+
+bank_csv = os.path.join("Resources", "Budget_data.csv")
+
+with open(bank_csv) as csvfile:
+    
+    csvreader = csv.reader(bank_csv, delimiter=",")
+    #csv_header = next(csvreader)
+    print(csvreader)
+
+    for row in csvreader:
+        print(row[1])
+
+
+#total_months =
+#net_total = 
+#avg_chage = 
+#greatest_increase = 
+#greatest_decrease = 
